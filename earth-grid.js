6ba0fb782280dd47fb5a90f3796ba5d2a2179936
Emmanuel Vendé <emmanuel@vende.fr>
2026-07-18 22:38:29 +0200

    Modularize some more code

--- earth-grid.js
+++ earth-grid.js
@@ -1,10 +1,10 @@
 import * as THREE from 'three';
 import { createLabel } from './utils.js';
 
-export function setupEarthGrid(earth, earthRadius) {
-    // ==========================================
-    // --- GRILLE DES MÉRIDIENS ET PARALLÈLES ---
-    // ==========================================
+/**
+ * meridians & parallels
+ */
+export function setupEarthGrid(earthGrob, earthRadius) {
     const gridGeometry = new THREE.SphereGeometry(earthRadius + 50, 36, 36);
     const edges = new THREE.EdgesGeometry(gridGeometry);
     const gridMaterial = new THREE.LineBasicMaterial({
@@ -16,17 +16,17 @@ export function setupEarthGrid(earth, earthRadius) {
     const earthGrid = new THREE.LineSegments(edges, gridMaterial);
 
     // On l'ajoute aussi à "earth" pour qu'il tourne avec !
-    earth.add(earthGrid);
+    earthGrob.add(earthGrid);
 
     // Méridien de Greenwich (0° Longitude, posé sur l'Équateur)
     const labelGreenwich = createLabel('0° (Greenwich)', '#00ffaa');
     labelGreenwich.position.set(earthRadius + 250, 0, 0); // Axe X de la Terre
-    earth.add(labelGreenwich);
+    earthGrob.add(labelGreenwich);
 
     // Longitude 90° Est
     const label90E = createLabel('90° E', '#00ffaa');
     label90E.position.set(0, 0, -(earthRadius + 250));
-    earth.add(label90E);
+    earthGrob.add(label90E);
 
     // Parallèle 45° Nord (par exemple)
     const label45N = createLabel('45° N', '#ffff00');
@@ -34,5 +34,5 @@ export function setupEarthGrid(earth, earthRadius) {
     const h45 = earthRadius * Math.sin(Math.PI / 4);
     const r45 = earthRadius * Math.cos(Math.PI / 4);
     label45N.position.set(r45 + 250, h45, 0);
-    earth.add(label45N);
+    earthGrob.add(label45N);
 }
--- main.js
+++ main.js
@@ -80,8 +80,8 @@ const earthMaterial = new THREE.MeshPhongMaterial({
     // emissive: new THREE.Color(0x001020)
 });
 
-const earth = new THREE.Mesh(earthGeometry, earthMaterial);
-scene.add(earth);
+const earthGrob = new THREE.Mesh(earthGeometry, earthMaterial);
+scene.add(earthGrob);
 
 // ==========================================
 // --- REPÈRE ECI (Axes X, Y, Z) ---
@@ -130,45 +130,10 @@ scene.add(equator);
 const axesHelperECF = new THREE.AxesHelper(9000);
 
 // TRÈS IMPORTANT : On l'ajoute à "earth", pas à "scene" !
-earth.add(axesHelperECF);
+earthGrob.add(axesHelperECF);
 
 
-/*
-// ==========================================
-// --- GRILLE DES MÉRIDIENS ET PARALLÈLES ---
-// ==========================================
-const gridGeometry = new THREE.SphereGeometry(earthRadius + 50, 36, 36);
-const edges = new THREE.EdgesGeometry(gridGeometry);
-const gridMaterial = new THREE.LineBasicMaterial({
-    color: 0x00ffaa,
-    transparent: true,
-    opacity: 0.4
-});
-
-const earthGrid = new THREE.LineSegments(edges, gridMaterial);
-
-// On l'ajoute aussi à "earth" pour qu'il tourne avec !
-earth.add(earthGrid);
-
-// Méridien de Greenwich (0° Longitude, posé sur l'Équateur)
-const labelGreenwich = createLabel('0° (Greenwich)', '#00ffaa');
-labelGreenwich.position.set(earthRadius + 250, 0, 0); // Axe X de la Terre
-earth.add(labelGreenwich);
-
-// Longitude 90° Est
-const label90E = createLabel('90° E', '#00ffaa');
-label90E.position.set(0, 0, -(earthRadius + 250));
-earth.add(label90E);
-
-// Parallèle 45° Nord (par exemple)
-const label45N = createLabel('45° N', '#ffff00');
-// On calcule la position en hauteur pour 45°
-const h45 = earthRadius * Math.sin(Math.PI / 4);
-const r45 = earthRadius * Math.cos(Math.PI / 4);
-label45N.position.set(r45 + 250, h45, 0);
-earth.add(label45N);
-*/
-setupEarthGrid(earth, earthRadius);
+setupEarthGrid(earthGrob, earthRadius);
 
 // --- LE SATELLITE ---
 const satSize = 500; // 500 km de côté :)
@@ -232,7 +197,7 @@ function animate() {
     const gmst = gstime(now);
 
     // Update Earth rotation accordingly
-    earth.rotation.y = gmst;
+    earthGrob.rotation.y = gmst;
 
     // Compute Satellite Earth Centered Inertial coordinates
     const positionAndVelocityECI = propagate(satRecWorldview3, now);
